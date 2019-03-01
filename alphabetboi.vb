fa1dc8f79eedb3e687c05ef306075b49659bd438
Tim Barber <tfbninja@users.noreply.github.com>
2019-03-01 12:45:47 -0700

    yee

---
+++ alphabetboi.vb
@@ -0,0 +1,33 @@
+Set WshShell = WScript.CreateObject("WScript.Shell")
+WshShell.Run "alphabet-typing-game-CherryPie-Day.py", 9
+
+' Give it time to load
+WScript.Sleep 5500
+
+WshShell.SendKeys " "
+WshShell.SendKeys "a"
+WshShell.SendKeys "b"
+WshShell.SendKeys "c"
+WshShell.SendKeys "d"
+WshShell.SendKeys "e"
+WshShell.SendKeys "f"
+WshShell.SendKeys "g"
+WshShell.SendKeys "h"
+WshShell.SendKeys "i"
+WshShell.SendKeys "j"
+WshShell.SendKeys "k"
+WshShell.SendKeys "l"
+WshShell.SendKeys "m"
+WshShell.SendKeys "n"
+WshShell.SendKeys "o"
+WshShell.SendKeys "p"
+WshShell.SendKeys "q"
+WshShell.SendKeys "r"
+WshShell.SendKeys "s"
+WshShell.SendKeys "t"
+WshShell.SendKeys "u"
+WshShell.SendKeys "v"
+WshShell.SendKeys "w"
+WshShell.SendKeys "x"
+WshShell.SendKeys "y"
+WshShell.SendKeys "z"
